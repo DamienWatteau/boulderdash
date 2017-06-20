2ddc61c4fb77a05583df18d737227852055dc62b
DamienWatteau <damien.watteau@viacesi.fr>
2017-06-20 15:30:10 +0200

    add class Menu

--- main/src/main/java/main/Main.java
+++ main/src/main/java/main/Main.java
@@ -1,12 +1,6 @@
 package main;
-import java.sql.SQLException;
 
-import javax.swing.text.View;
 
-import controller.ControllerFacade;
-import model.ModelFacade;
-import model.dao.AbstractDAO;
-import view.ViewFacade;
 
 /**
  * <h1>The Class Main.</h1>
@@ -23,30 +17,16 @@ public abstract class Main {
      *            the arguments
      */
     public static void main(final String[] args) {
-<<<<<<< HEAD
-    	ViewFacade view = new ViewFacade();
-    	view.Pseudo();
-    	
-    	
-    	//view.Pseudo();
-    	/*final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
-
-=======
-    	AbstractDAO dao = new AbstractDAO();
-    	dao.open();
-    	/*        final ControllerFacade controller = new ControllerFacade(new ViewFacade(), new ModelFacade());
->>>>>>> 119c1e2e1a7a27bff74acf435a5568fd0a0c32eb
-        try {
-            controller.start();
-        } catch (final SQLException exception) {
-            exception.printStackTrace();
-<<<<<<< HEAD
-        }*/
-    }
-=======
-        }
-    }*/
->>>>>>> 119c1e2e1a7a27bff74acf435a5568fd0a0c32eb
+    	 Menu menu = new Menu();
+  	   BddRequete requete = new BddRequete();
+  	   requete.user(menu.MenuPseudo());
+  	   menu.MenuLevel();
+  	   
+  		JFrame fen = new JFrame();
+  	   
+  		BddRequete requete2 = new BddRequete();
+  		requete2.selectTable();	
+  		
 
     }
 }
